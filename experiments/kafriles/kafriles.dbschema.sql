0e29890b95b3cd6df8a6280408431f260d17ca77
nanga <mallinanga@gmail.com>
2012-07-28 01:02:19 +0300

    Ignored experiments

--- experiments/kafriles/kafriles.dbschema.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE `kafriles` (
-  `id` tinyint(3) unsigned NOT NULL auto_increment,
-  `header` text,
-  `body` text,
-  `timeGenerated` date default NULL,
-  PRIMARY KEY  (`id`)
-) TYPE=MyISAM PACK_KEYS=0 AUTO_INCREMENT=1 ;
